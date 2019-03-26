151e3001a25832cd531f86c43e117b8cff1203cc
IgoR-NiK <Igor_2501@mail.ru>
2019-03-26 21:54:56 +0300

    Удалено всё лишнее

--- UniversitySchedule/Startup.cs
+++ UniversitySchedule/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -46,7 +47,15 @@ namespace UniversitySchedule
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseStaticFiles();
+            app.UseStatusCodePagesWithReExecute("/Home");
+
+            var provider = new FileExtensionContentTypeProvider();
+            provider.Mappings[".vue"] = "text/html";
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                ContentTypeProvider = provider
+            });
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
@@ -54,7 +63,7 @@ namespace UniversitySchedule
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            });            
         }
     }
 }
