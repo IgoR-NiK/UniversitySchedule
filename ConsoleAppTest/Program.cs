aa1d6cc9841b9791f0ff602adb53ed0f548aa8eb
IgoR-NiK <Igor_2501@mail.ru>
2019-04-07 12:24:57 +0300

    Добавлен консольный проект для тестирования алгоритма

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using DataLayer;
+
+namespace ConsoleAppTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var algorithm = new Algorithm();
+
+			Console.WriteLine("Hello World!");
+			Console.WriteLine(algorithm.Run());
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ DataLayer/Algorithm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer
+{
+	public class Algorithm
+	{
+		public string Run()
+		{
+			return "Алгоритм запущен";
+		}
+	}
+}
